3f8863f7be346dcdb81b273b7274d17ce6a92705
luhaomeng <l84412600@china.huawei.com>
2025-08-28 17:27:58 +0800

    Greeting message for ChioChat

---
+++ src/main/java/ChioChat.java
@@ -0,0 +1,11 @@
+public class ChioChat {
+    public static void main(String[] args) {
+        String greatingMSG = "____________________________________________________________\n"
+                + " Hello! I'm ChioChat\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(greatingMSG);
+    }
+}
